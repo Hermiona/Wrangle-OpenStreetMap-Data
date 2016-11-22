f6587c24d333c43278d29bab1030600f7f50b464
NadiyaSitdykova <rovenskasa@gmail.com>
2016-11-22 15:19:20 -0800

    add postcode cleaner

--- postcode.py
+++ postcode.py
@@ -18,3 +18,15 @@ def audit_postcode(filename):
                                 unexpected_postcodes[postcode] = 0
                             unexpected_postcodes[postcode] += 1
     return unexpected_postcodes
+
+def clear_postcode(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    for child in root:
+        if child.tag == "node" or child.tag == "way":
+            for tag in child.iter("tag"):
+                if tag.attrib['k'] == "addr:postcode":
+                    postcode = tag.attrib['v']
+                    if not is_valid_postcode(postcode):
+                        child.remove(tag)
+    tree.write("cleared_postcode_" + filename, encoding='utf-8')
