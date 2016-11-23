24b508180eb7349f3e0dee63774d034fe969c604
NadiyaSitdykova <rovenskasa@gmail.com>
2016-11-23 15:17:29 -0800

    add street cleaning

--- street.py
+++ street.py
@@ -14,6 +14,8 @@ street_type_mapping = {
             unicode("Str.", 'utf8'): unicode("улица", 'utf8'),
             unicode("St", 'utf8'): unicode("улица", 'utf8'),
             unicode("St.", 'utf8'): unicode("улица", 'utf8'),
+            unicode("Улица", 'utf8'): unicode("улица", 'utf8'),
+            unicode("улица", 'utf8'): unicode("улица", 'utf8'),
             unicode("с.", 'utf8'): unicode("село", 'utf8'),
             unicode("мкр", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("мкр.", 'utf8'): unicode("микрорайон", 'utf8'),
@@ -21,6 +23,7 @@ street_type_mapping = {
             unicode("микра", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("Микрорайон", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("микрарайон", 'utf8'): unicode("микрорайон", 'utf8'),
+            unicode("микрорайон", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("микраройон", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("микрорайно", 'utf8'): unicode("микрорайон", 'utf8'),
             unicode("микрораон", 'utf8'): unicode("микрорайон", 'utf8'),
@@ -29,11 +32,12 @@ street_type_mapping = {
             unicode("прe", 'utf8'): unicode("переулок", 'utf8'),
             unicode("пре.", 'utf8'): unicode("переулок", 'utf8'),
             unicode("перулок", 'utf8'): unicode("переулок", 'utf8'),
+            unicode("переулок", 'utf8'): unicode("переулок", 'utf8'),
+            unicode("проспект", 'utf8'): unicode("проспект", 'utf8'),
             unicode("Strasse", 'utf8'): unicode("проспект", 'utf8'),
             unicode("Avenue", 'utf8'): unicode("проспект", 'utf8'),
             unicode("blvrd.", 'utf8'): unicode("бульвар", 'utf8')
             }
-
 manual_street_name_mapping = {
     unicode("Джал-15 (Джал Артис)", 'utf8'): unicode("Джал Арча микрорайон", 'utf8'),
     unicode("Озёрная", 'utf8'): unicode("Oзерная улица", 'utf8'),
@@ -47,7 +51,7 @@ manual_street_name_mapping = {
     unicode("Тыналиева", 'utf8'): unicode("Тыналиева улица", 'utf8'),
     unicode("он-арча", 'utf8'): unicode("Он Арча улица", 'utf8'),
     unicode("Карла Маркса", 'utf8'): unicode("Юнусалиева проспект", 'utf8'),
-    unicode("Летняя", 'utf8'): unicode("Улица летняя", 'utf8'),
+    unicode("Летняя", 'utf8'): unicode("Летняя улица", 'utf8'),
     unicode("Белинского", 'utf8'): unicode("Манаса проспект", 'utf8'),
     unicode("Горький - Матросов", 'utf8'): unicode("Горького улица", 'utf8'),
     unicode("Жилгородок совмина", 'utf8'): unicode("Совмина микрорайон", 'utf8'),
@@ -55,14 +59,16 @@ manual_street_name_mapping = {
     unicode("Т-Молдо", 'utf8'): unicode("Т. Молдо улица", 'utf8'),
     unicode("Спортивная", 'utf8'): unicode("Спортивная улица", 'utf8'),
     unicode("Горький", 'utf8'): unicode("Горького улица", 'utf8'),
-    unicode("Жилой комплекс \"Юг-7\"", 'utf8'): unicode("Юг-7 микрорайон", 'utf8')
+    unicode("Жилой комплекс \"Юг-7\"", 'utf8'): unicode("Юг-7 микрорайон", 'utf8'),
     unicode("Ореховая", 'utf8'): unicode("Ореховая улица", 'utf8'),
     unicode("Скобелевская", 'utf8'): unicode("Скобелевская улица", 'utf8'),
     unicode("Мадиева", 'utf8'): unicode("Мадиева улица", 'utf8'),
     unicode("Сухэ-Батора", 'utf8'): unicode("Сухэ-Батора улица", 'utf8'),
     unicode("Восток-5 д. 1/2 Апам", 'utf8'): unicode("Восток-5 микрорайон", 'utf8'),
     unicode("Совхозная", 'utf8'): unicode("Совхозная улица", 'utf8'),
-    unicode("Юлиуса Фучика", 'utf8'): unicode("Ю. Фучика улица", 'utf8')
+    unicode("Юлиуса Фучика", 'utf8'): unicode("Ю. Фучика улица", 'utf8'),
+    unicode("Жылга-Сай", 'utf8'): unicode("Жылга-Сай улица", 'utf8'),
+    unicode("Дабан", 'utf8'): unicode("Дабан улица", 'utf8')
 }
 
 
@@ -78,36 +84,81 @@ def get_expected_streets(url):
         expected_streets.add(street)
     return expected_streets
 
-def audit_street_names(streets, street_name):
-    street_type = unicode(street_name.strip().split(" ")[-1])
-    if street_type not in expected_street_types and street_type not in street_type_mapping:
-        street_type = unicode(street_name.strip().split(" ")[0])
-        if street_type not in expected_street_types and street_type not in street_type_mapping:
-            street_type_prefix = unicode(street_name.strip().split(" ")[0][:4])
-            if street_type_prefix not in expected_street_types and street_type_prefix not in street_type_mapping:
-                street_type_suffix = unicode(street_name.strip().split(" ")[0][-3:])
-                if street_type_suffix not in expected_street_types and street_type_suffix not in street_type_mapping:
-                    expected_streets = get_expected_streets(kyrgyz_post_url)
-                    street_is_expected = False
-                    for street in expected_streets:
-                        if street.find(street_name.strip()) != -1:
-                            street_is_expected = True
-                            expected_street = street
-                            break
-                    if not street_is_expected:
-                        #streets.append(unicode(street_name + " " + expected_street))
-                        #print streets[-1]
-                        streets.add(unicode(street_name))
-
 def audit_street(filename):
-    streets = set()
+    unexpected_streets = set()
     with open(filename, 'r') as file:
         for _ , elem in ET.iterparse(file, events=("start",)):
             if elem.tag == "node" or elem.tag == "way":
                 for tag in elem.iter("tag"):
                     if tag.attrib['k'] == "addr:street":
-                        audit_street_names(streets, tag.attrib['v'])
-    return streets
+                        raw_street = tag.attrib['v']
+                        if unicode(raw_street.strip().split(" ")[-1]) not in expected_street_types:
+                            unexpected_streets.add(raw_street)
+    return unexpected_streets
+
+# Takes a string of raw street and returns it with street type in standard format if possible. Otherwise returns None.
+def fix_street_type(raw_street):
+    if raw_street == None:
+        return None
+    # check if the street type in the end of raw street
+    street_type = unicode(raw_street.strip().split(" ")[-1])
+    street_without_type = " ".join(unicode(raw_street.strip().split(" ")[:-1])) + " "
+    if street_type not in street_type_mapping:
+        # check if the street type in the beginning of raw street
+        street_type = unicode(raw_street.strip().split(" ")[0])
+        street_without_type = " ".join(unicode(raw_street.strip().split(" ")[1:])) + " "
+        if street_type not in street_type_mapping:
+            # check if the street type is the abbreviation in the end of raw street
+            street_type = unicode(raw_street.strip()[:4])
+            street_without_type = unicode(raw_street.strip()[4:]) + " "
+            if street_type not in street_type_mapping:
+                # check if the street type is the abbreviation in the beginning of raw street
+                street_type = unicode(raw_street.strip()[-3:])
+                street_without_type = unicode(raw_street.strip()[:-3]) + " "
+                if street_type not in street_type_mapping:
+                    return None
+    return street_without_type + street_type_mapping[street_type]
+
+
+# Takes a string of raw street and returns it in standard format. Manually prepared fixing is performing for existing raw streets.
+# However, for non existing streets function returns None.
+def fix_street(raw_street, expected_streets):
+    # Check manually prepared map of existing raw streets
+    if raw_street in manual_street_name_mapping:
+        return manual_street_name_mapping[raw_street]
+    # Try to fix street type to standard format
+    fixed_street = fix_street_type(raw_street)
+    if fixed_street == None:
+        # Try to get street type from expected streets (from kyrgyz post website)
+        expected_street = None
+        for street in expected_streets:
+            if street.find(raw_street.strip()) != -1:
+                expected_street = street
+                break
+        fixed_street = fix_street_type(expected_street)
+        if fixed_street == None:
+            return None
+    return fixed_street
+
+
+
+def clean_street(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    expected_streets = get_expected_streets(kyrgyz_post_url)
+    for child in root:
+        if child.tag == "node" or child.tag == "way":
+            for tag in child.iter("tag"):
+                if tag.attrib['k'] == "addr:street":
+                    raw_street = tag.attrib['v']
+                    if unicode(raw_street.strip().split(" ")[-1]) not in expected_street_types:
+                        fixed_street = fix_street(raw_street, expected_streets)
+                        if fixed_street == None:
+                            #print(raw_street + " is not a street")
+                            child.remove(tag)
+                        else:
+                            tag.attrib['v'] = fixed_street
+    tree.write("cleaned_street_" + filename, encoding='utf-8')
 
 
 
