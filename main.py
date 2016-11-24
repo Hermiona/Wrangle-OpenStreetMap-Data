962c5a66e04737d603f1468b309950488215ec30
NadiyaSitdykova <rovenskasa@gmail.com>
2016-11-23 16:01:12 -0800

    add calls of cleaning functions to main

--- main.py
+++ main.py
@@ -5,27 +5,40 @@ import phone
 import website
 
 if __name__ == '__main__':
-    audit_option = sys.argv[1]
-    audit_filename = sys.argv[2]
+    operation_option = sys.argv[1]
+    data_option = sys.argv[2]
+    filename = sys.argv[3]
     UTF8Writer = codecs.getwriter('utf8')
     sys.stdout = UTF8Writer(sys.stdout)
-    if audit_option == "street":
-        street_types = street.audit_street(audit_filename)
-        for street_type in street_types.keys():
-            print(street_type)
-            for street_name in street_types[street_type]:
-                print("\t" + street_name)
-    elif audit_option == "postcode":
-        post_codes = postcode.audit_postcode(audit_filename)
-        for code, count in post_codes.items():
-            print(code + " | " + str(count))
-    elif audit_option == "phone":
-        phones = phone.audit_phone(audit_filename)
-        for phone in phones:
-            print(phone)
-    elif audit_option == "website":
-        websites = website.audit_website(audit_filename)
-        for website in websites:
-            print(website)
+    if operation_option == "clean":
+        if data_option == "street":
+            street.clean_street(filename)
+        elif data_option == "postcode":
+            postcode.clean_postcode(filename)
+        elif data_option == "phone":
+            phone.clean_phone(filename)
+        elif data_option == "website":
+            website.clean_website(filename)
+        else:
+            print("Incorrect data option. Choose from: street, postcode, phone, website.")
+    elif operation_option == "audit":
+        if data_option == "street":
+            streets = street.audit_street(filename)
+            for street in streets:
+                print(street)
+        elif data_option == "postcode":
+            postcodes = postcode.audit_postcode(filename)
+            for code, count in postcodes.items():
+                print(code + " | " + str(count))
+        elif data_option == "phone":
+            phones = phone.audit_phone(filename)
+            for phone in phones:
+                print(phone)
+        elif data_option == "website":
+            websites = website.audit_website(filename)
+            for website in websites:
+                print(website)
+        else:
+            print("Incorrect data option. Choose from: street, postcode, phone, website.")
     else:
-        print "Incorrect audit option. Choose from: street, postcode, phone, website."
+        print("Incorrect operation option. Choose from: audit, clean.")
