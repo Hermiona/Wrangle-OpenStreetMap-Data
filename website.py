ca9dc0577c53f09b048fbd43d1679a457658211e
NadiyaSitdykova <rovenskasa@gmail.com>
2016-11-28 18:15:33 -0800

    refactor and fix website cleaning

--- website.py
+++ website.py
@@ -4,7 +4,10 @@ import requests
 
 # Regex for website address in standard format
 # (which follows best practices from https://wiki.openstreetmap.org/wiki/Key:website)
-website_re = re.compile(r'https?://[a-z0-9\./]*')
+good_website = re.compile(r'https?://[a-z0-9\./]*')
+
+def url_is_good(url):
+    return website_re.match(url)
 
 # Returns list of websites which are not in standard format
 def audit_website(filename):
@@ -28,9 +31,7 @@ def clean_website(filename):
             for tag in child.iter("tag"):
                 if tag.attrib['k'] == "website":
                     website = tag.attrib['v']
-                    if not website_re.match(website):
-                        if len(website) < 3 or website[:3] != "www":
-                            website = "www." + website
+                    if not url_is_good(website):
                         tag.attrib['v'] = "http://" + website
 
                         #possible validation of websites
